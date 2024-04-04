6bfa139a53ef11d71d17f74b5062b3ba00b81034
Luis_Enrique_Ledesma_Ollague <luisenric32@gmail.com>
2024-04-04 15:54:41 +0200

    Implementar Auth0

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,19 +6,27 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { RegisterComponent } from './components/register/register.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { AuthButtonComponent } from './components/auth-button/auth-button.component';
+import { provideAuth0 } from '@auth0/auth0-angular';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     RegisterComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    AuthButtonComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [
+    provideAuth0({
+      domain: 'dev-yyzuj3kafug18e38.eu.auth0.com',
+      clientId: 'QOiV3m6kyD74336XSGU49LOcfiktUp2T',
+      authorizationParams: {
+        redirect_uri: window.location.origin,
+      },
+    }),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
