523f940ea2b85981b3c9d7960a9b7e2d17acb012
julijuco <jort166@hotmail.com>
2024-04-04 13:44:09 +0200

    creo menu de telefono y hago home

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { RegisterComponent } from './components/register/register.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { MenuMovileComponent } from './components/menu-movile/menu-movile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     RegisterComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    MenuMovileComponent
   ],
   imports: [
     BrowserModule,
