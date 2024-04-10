5f7fd87f0f6c322b36a94b72efab2fe0a1172506
christiann26 <christiansastre74@gmail.com>
2024-04-10 13:24:21 +0200

    Video Player V2.1

--- frontend/src/app/components/player/player.component.ts
+++ frontend/src/app/components/player/player.component.ts
@@ -10,39 +10,22 @@ export class PlayerComponent {
   constructor() { }
   items = [
     { image: '../../../assets/img/sacothai.png', title: 'Entreno con saco #1.', category: 'saco' },
-    { image: '../../../assets/img/sacothai.png', title: 'Entreno con saco #2.', category: 'saco' },
-    { image: '../../../assets/img/sacothai.png', title: 'Entreno con saco #3.', category: 'saco' }
-  ];
-
-  items2 = [
-    { image: '../../../assets/img/pareja.png', title: 'Entreno con pareja #1.', category: 'pareja' },
-    { image: '../../../assets/img/pareja.png', title: 'Entreno con pareja #2.', category: 'pareja' },
-    { image: '../../../assets/img/pareja.png', title: 'Entreno con pareja #3.', category: 'pareja' },
-  ];
-
-  items3 = [
-    { image: '../../../assets/img/conequipamiento.png', title: 'Entreno con equipamiento #1.', category: 'con-equipamiento' },
-    { image: '../../../assets/img/conequipamiento.png', title: 'Entreno con equipamiento #2.', category: 'con-equipamiento' },
     { image: '../../../assets/img/conequipamiento.png', title: 'Entreno con equipamiento #3.', category: 'con-equipamiento' },
-  ];
-  
-  items4 = [
-    { image: '../../../assets/img/sombrathai.png', title: 'Entreno sin equipamiento #1.', category: 'sin-equipamiento' },
+    { image: '../../../assets/img/pareja.png', title: 'Entreno con pareja #1.', category: 'pareja' },
     { image: '../../../assets/img/sombrathai.png', title: 'Entreno sin equipamiento #2.', category: 'sin-equipamiento' },
-    { image: '../../../assets/img/sombrathai.png', title: 'Entreno sin equipamiento #3.', category: 'sin-equipamiento' },
-  ];
-  item5 = [
-    { image: '../../../assets/img/saco.png', title: 'Entreno con saco #1.', category: 'saco' },
-    { image: '../../../assets/img/saco.png', title: 'Entreno con saco #2.', category: 'saco' },
-    { image: '../../../assets/img/saco.png', title: 'Entreno con saco #3.', category: 'saco' }
-  ];
-
-  items6 = [
+    { image: '../../../assets/img/saco.png', title: 'Entreno con saco #3.', category: 'saco' },
     { image: '../../../assets/img/boxeopareja.png', title: 'Entreno con pareja #1.', category: 'pareja' },
-    { image: '../../../assets/img/boxeopareja.png', title: 'Entreno con pareja #2.', category: 'pareja' },
-    { image: '../../../assets/img/boxeopareja.png', title: 'Entreno con pareja #3.', category: 'pareja' },
+    { image: '../../../assets/img/boxeoequimapiento.png', title: 'Entreno con equipamiento #1.', category: 'con-equipamiento' },
+    { image: '../../../assets/img/sinequipamiento.png', title: 'Entreno sin equipamiento #3.', category: 'sin-equipamiento' },
   ];
-
-  allItems = [...this.items, ...this.items2, ...this.items3,...this.items4,...this.item5,...this.items6];
+    comentarios = [
+      {texto:'Ay amiga que guapo es el entrenador'},
+      {texto:'Joel te odio mucho nunca te rindas'}, 
+      {texto:'Ostia no he visto nada de nada ayuda'}, 
+      {texto:'Me ha ayudado mucho a mejorar ahora nadie me para'}, 
+      {texto:'El entrenador va un poco fumadete pero se le ve majo'}  
+    ];
+    
+  
 
 }
