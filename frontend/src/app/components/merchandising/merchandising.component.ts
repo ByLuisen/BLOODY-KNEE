3249916e4dd78744473a7c2eec2b9adc4edcb798
christiann26 <christiansastre74@gmail.com>
2024-04-12 11:26:30 +0200

    merchan actualizado

--- frontend/src/app/components/merchandising/merchandising.component.ts
+++ frontend/src/app/components/merchandising/merchandising.component.ts
@@ -12,4 +12,12 @@ export class MerchandisingComponent {
     const minValue = parseInt(this.priceRangeInput.nativeElement.value);
     const maxValue = 150 - minValue;
   }
+  productos = [
+    { nombre: 'Guantes Buddha', imagen: '../../../assets/img/guantes.png', precio: 15.00 },
+    { nombre: 'Comba', imagen: '../../../assets/img/comba.png', precio: 28.00 },
+    { nombre: 'Mochila 2', imagen: '../../../assets/img/mochila2.png', precio: 12.00 },
+    { nombre: 'Vendas', imagen: '../../../assets/img/vendas.png', precio: 18.00 },
+    { nombre: 'Mochila', imagen: '../../../assets/img/mochila.png', precio: 34.00 },
+    { nombre: 'Llavero', imagen: '../../../assets/img/llavero.png', precio: 30.00 }
+  ];
 }
