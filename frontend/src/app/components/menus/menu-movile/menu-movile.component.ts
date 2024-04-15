ac85363cb0b76a81ee9c8c52bd66b9d95adcb87c
julijuco <jort166@hotmail.com>
2024-04-15 13:37:50 +0200

    estoy muerto

--- frontend/src/app/components/menus/menu-movile/menu-movile.component.ts
+++ frontend/src/app/components/menus/menu-movile/menu-movile.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, HostListener, Inject } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-menu-movile',
@@ -6,5 +8,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu-movile.component.css']
 })
 export class MenuMovileComponent {
+  expanded: boolean = false;
+  
+  constructor(@Inject(DOCUMENT) public document: Document, public auth: AuthService) {}
 
+  expandDiv() {
+    const div = document.getElementById("expandibleDiv")!;
+    const button = document.querySelector(".fixed-button") as HTMLElement;
+
+    if (!this.expanded) {
+      // Cambiar posición del botón al centro
+      button.style.bottom = "50%";
+      button.style.right = "50%";
+      button.style.transform = "translate(50%, 50%)";
+
+      // Expandir el div
+      div.style.height = "340px"; // Cambia este valor al deseado
+    } else {
+      // Revertir los cambios en el botón y el div
+      button.style.bottom = "40px";
+      button.style.right = "40px";
+      button.style.transform = "none";
+      div.style.height = "0";
+    }
+    this.expanded = !this.expanded;
+  }
+
+  @HostListener('document:click', ['$event'])
+  clickOutside(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    const div = document.getElementById("expandibleDiv")!;
+
+    // Si se hace clic fuera del div expandible, revertir los cambios en el botón y el div
+    if (div.contains(target) && this.expanded) {
+      this.expandDiv();
+    }
+  }
 }
