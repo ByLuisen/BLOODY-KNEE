21cfb67aa5f2c95e232309f752d143e4ccf74b0a
julijuco <ortegarodera1606@gmail.com>
2024-04-25 13:45:27 +0200

    Dinamizar productos

--- backend/database/seeders/ProductSeeder.php
+++ backend/database/seeders/ProductSeeder.php
@@ -16,14 +16,39 @@ class ProductSeeder extends Seeder
     public function run(): void
     {
         // Crear productos ficticios
+
+        /**
+         * Indice de marcas y categorias
+         * ID   =>    Tipo (Brands)
+         * 1    =>  Venum
+         * 2    =>  Buddha
+         * 3    =>  Rival
+         * 4    =>  Nike
+         *
+         * ID   =>    Tipo (Category)
+         * 1    =>      Ropa
+         * 2    =>      Equipamiento
+         * 3    =>      Accesorios
+         */
+
         Product::create([
             'brand_id' => 1,
             'category_id' => 1,
-            'name' => 'Producto 1',
+            'name' => '',
             'description' => 'Descripción del Producto 1',
             'price' => 100,
             'stock' => 50,
-            'url_img' => 'url_de_la_imagen_del_producto_1.jpg'
+            'url_img' => '.jpg'
+        ]);
+
+        Product::create([
+            'brand_id' => 2,
+            'category_id' => 2,
+            'name' => 'Producto 2',
+            'description' => 'Descripción del Producto 2',
+            'price' => 150,
+            'stock' => 30,
+            'url_img' => 'url_de_la_imagen_del_producto_2.jpg'
         ]);
 
         Product::create([
