bb236431edcb4008c8b31e88adfba2db6967225d
julijuco <ortegarodera1606@gmail.com>
2024-04-26 13:04:26 +0200

    Product seeder

--- backend/database/seeders/ProductSeeder.php
+++ backend/database/seeders/ProductSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Product;
+
 /**
  *
  */
@@ -18,6 +19,7 @@ class ProductSeeder extends Seeder
         // Crear productos ficticios
 
         /**
+         *
          * Indice de marcas y categorias
          * ID   =>    Tipo (Brands)
          * 1    =>  Venum
@@ -34,30 +36,130 @@ class ProductSeeder extends Seeder
         Product::create([
             'brand_id' => 1,
             'category_id' => 1,
-            'name' => '',
-            'description' => 'Descripción del Producto 1',
-            'price' => 100,
+            'name' => 'Jogger venum',
+            'description' => '',
+            'price' => 75.50,
             'stock' => 50,
             'url_img' => '.jpg'
         ]);
 
         Product::create([
-            'brand_id' => 2,
+            'brand_id' => 1,
             'category_id' => 2,
-            'name' => 'Producto 2',
-            'description' => 'Descripción del Producto 2',
-            'price' => 150,
-            'stock' => 30,
+            'name' => 'Venum Backpack',
+            'description' => '',
+            'price' => 65.30,
+            'stock' => 50,
             'url_img' => 'url_de_la_imagen_del_producto_2.jpg'
         ]);
 
         Product::create([
-            'brand_id' => 2,
+            'brand_id' => 1,
             'category_id' => 2,
-            'name' => 'Producto 2',
+            'name' => 'Venum Xtream Backpack',
             'description' => 'Descripción del Producto 2',
-            'price' => 150,
+            'price' => 130,
             'stock' => 30,
+            'url_img' => '.jpg'
+        ]);
+
+        Product::create([
+            'brand_id' => 1,
+            'category_id' => 2,
+            'name' => 'Venum Sportbag',
+            'description' => 'Descripción del Producto 2',
+            'price' => 89.99,
+            'stock' => 30,
+            'url_img' => '.jpg'
+        ]);
+
+        Product::create([
+            'brand_id' => 1,
+            'category_id' => 3,
+            'name' => 'Venum Cap Sand',
+            'description' => 'Descripción del Producto 2',
+            'price' => 89.99,
+            'stock' => 30,
+            'url_img' => '.jpg'
+        ]);
+
+        Product::create([
+            'brand_id' => 1,
+            'category_id' => 3,
+            'name' => 'Venum Cap Sand',
+            'description' => 'Descripción del Producto 2',
+            'price' => 25.50,
+            'stock' => 30,
+            'url_img' => '.jpg'
+        ]);
+
+        Product::create([
+            'brand_id' => 1,
+            'category_id' => 3,
+            'name' => 'Venum Cap Brown',
+            'description' => 'Descripción del Producto 2',
+            'price' => 25.50,
+            'stock' => 30,
+            'url_img' => '.jpg'
+        ]);
+
+        Product::create([
+            'brand_id' => 1,
+            'category_id' => 3,
+            'name' => 'Venum Cap Dark Brown',
+            'description' => 'Descripción del Producto 2',
+            'price' => 25.50,
+            'stock' => 30,
+            'url_img' => '.jpg'
+        ]);
+
+        Product::create([
+            'brand_id' => 1,
+            'category_id' => 2,
+            'name' => 'Guantes Piel Edition',
+            'description' => 'Descripción del Producto 2',
+            'price' => 79.99,
+            'stock' => 30,
+            'url_img' => '.jpg'
+        ]);
+
+        Product::create([
+            'brand_id' => 1,
+            'category_id' => 2,
+            'name' => 'Guantes Fantasy Edition',
+            'description' => 'Descripción del Producto 2',
+            'price' => 49.99,
+            'stock' => 30,
+            'url_img' => '.jpg'
+        ]);
+
+        Product::create([
+            'brand_id' => 1,
+            'category_id' => 2,
+            'name' => 'Guantes Venum MMA',
+            'description' => 'Descripción del Producto 2',
+            'price' => 47.99,
+            'stock' => 50,
+            'url_img' => 'url_de_la_imagen_del_producto_2.jpg'
+        ]);
+
+        Product::create([
+            'brand_id' => 1,
+            'category_id' => 1,
+            'name' => 'Camiseta Venum Bronze',
+            'description' => 'Descripción del Producto 2',
+            'price' => 29.99,
+            'stock' => 50,
+            'url_img' => 'url_de_la_imagen_del_producto_2.jpg'
+        ]);
+
+        Product::create([
+            'brand_id' => 1,
+            'category_id' => 1,
+            'name' => 'Camiseta Venum Negra',
+            'description' => 'Descripción del Producto 2',
+            'price' => 24.99,
+            'stock' => 50,
             'url_img' => 'url_de_la_imagen_del_producto_2.jpg'
         ]);
 
