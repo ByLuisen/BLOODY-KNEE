050585b2521844eb7dd1dfbc24ab943627e4e721
christiann26 <christiansastre74@gmail.com>
2024-05-23 19:32:28 +0200

    Backend Comentado

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+// Import necessary traits and classes
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -12,6 +12,7 @@ use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
+    // Include traits for API tokens, factory, notifications, and roles
     use HasApiTokens, HasFactory, Notifiable,  HasRoles;
 
     /**
@@ -44,34 +45,39 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    // Define a many-to-many relationship with the Quote model
     public function quotes()
     {
         return $this->belongsToMany(Quote::class)
             ->withTimestamps();
     }
 
+    // Define a many-to-many relationship with the Video model for visited videos
     public function videos()
     {
         return $this->belongsToMany(Video::class, 'user_visit_videos', 'user_id', 'video_id')->withTimestamps();
     }
 
+    // Define a many-to-many relationship with the Video model for favorite videos
     public function favorites()
     {
         return $this->belongsToMany(Video::class, 'user_favorite_videos', 'user_id', 'video_id')->withTimestamps();
     }
 
+    // Define a many-to-many relationship with the Video model for liked videos
     public function likes()
     {
         return $this->belongsToMany(Video::class, 'user_like_dislike_videos', 'user_id', 'video_id')->withTimestamps();
     }
 
+    // Define a one-to-one relationship with the Cart model
     public function cart()
     {
         return $this->hasOne(Cart::class);
     }
 
     /**
-     * Get the orders.
+     * Get the orders associated with the user.
      */
     public function orders(): HasMany
     {
